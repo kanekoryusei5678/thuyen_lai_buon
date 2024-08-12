dc4e2b365e2eb587e6c2848d3526631ab2351148
kanekoryusei5678 <kanekoryusei5678@github.com>
2024-08-12 23:41:40 +0700

    init

---
+++ a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e
+// @namespace    https://github.com/kanekoryusei5678/thuyen_lai_buon/raw/main/a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e.user.js
+// @version      2024.08.12
+// @description  down a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e
+// @author       You
+// @include      /^https:\/\/www.[a@]nim[a@]teb[o0]okst[o0]re.c[o0]m\/viewer\/.+/
+// @icon         https://raw.githubusercontent.com/kanekoryusei5678/thuyen_lai_buon/main/a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e.png
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/kanekoryusei5678/thuyen_lai_buon/raw/main/a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e.user.js
+// @updateURL    https://github.com/kanekoryusei5678/thuyen_lai_buon/raw/main/a-n-i-m-a-t-e-b-o-o-k-s-t-o-r-e.meta.js
+// ==/UserScript==
